5ce21ba680af902ee544c79a77fc82f00d40091e
Amaury LEBOUTEUX <amaury.lebouteux@gmail.com>
2019-10-16 16:20:41 +0200

    solution-react-5

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import './App.css';
-import Contact from './components/Contact';
+
 import ContactList from './components/ContactList';
 
-const Susan = {
-  image: "https://randomuser.me/api/portraits/women/34.jpg",
-  name: "Susan Neal",
-  online: true
-}
-
-const Lonnie = {
-  image: "https://randomuser.me/api/portraits/men/52.jpg",
-  name: "Lonnie Mitchell",  
-  online: false
-}
-
-const Vivan = {
-  image: "https://randomuser.me/api/portraits/women/82.jpg",
-  name: "Vivan Hunter",  
-  online: true
-}
 
 function App() {
   return (
     <div className="App">
-      <Contact {...Susan}/>
-      <Contact {...Lonnie}/>
-      <Contact {...Vivan}/>
+      
       <ContactList/>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,36 @@ import React from 'react';
 import './Contact.css';
 
 
-function Contact(props) {
-    return (
-        <div className="Contact">
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {online:props.online};
+    }
+    render(){
+        return (
+         <div className="Contact">
 
-            <img className="avatar" src={props.image} alt={props.name} />
+                <img className="avatar" src={this.props.image} alt={this.props.name} />
 
 
-            <div className="status">
-                <p className="name">{props.name}</p>
-                {props.online ? <span className="status-online"></span> : <span className="status-offline"></span>}
-                <span className="status-text">{props.online ? "Online" : "Offline"}</span>
+                <div className="status" onClick = {event => {
+                        let newStatus = !this.state.online;
+                        this.setState  ({online : newStatus });
+                    }}>
+                    <p className="name">{this.props.name}</p>
+                    
+                    {this.state.online ? <span className="status-online"></span> : <span className="status-offline"></span>}
+                    <span  className="status-text">{this.state.online ? "Online" : "Offline"}</span>
+                    
+                </div>
+
+
+
+
             </div>
+        );
 
-
-
-
-        </div>
-
-    );
+    }
 
 }
 
